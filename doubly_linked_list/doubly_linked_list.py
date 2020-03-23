d3d8cb987005052f1ab43478a1e205d64f55ca9a
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-23 15:28:07 -0700

    done with dll_queue.py

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -102,13 +102,23 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -123,7 +133,7 @@ class DoublyLinkedList:
             self.tail = None
         # if head
         elif node is self.head:
-            self.head = self.head.next #or node.next
+            self.head = self.head.next  # or node.next
             node.delete()
         # if tail
         elif node is self.tail:
@@ -136,4 +146,14 @@ class DoublyLinkedList:
     """Returns the highest value currently in the list"""
 
     def get_max(self):
-        pass
+        # Loop through all nodes, looking for biggest value
+        # TODO: Error checking
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
